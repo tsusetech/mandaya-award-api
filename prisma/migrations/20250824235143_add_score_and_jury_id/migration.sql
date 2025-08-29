1cd3b7b6104462922628abed3ff5f8e4d9402f6b
MUHAMMAD RIFQIH ALZIBRAH <rifqihalzibrahmuhammad@gmail.com>
2025-08-29 19:37:32 +0700

    fix

--- prisma/migrations/20250824235143_add_score_and_jury_id/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."QuestionResponse" ADD COLUMN     "score" DECIMAL(65,30);
-
--- AlterTable
-ALTER TABLE "public"."ResponseSession" ADD COLUMN     "juryId" INTEGER;
-
--- CreateIndex
-CREATE INDEX "ResponseSession_juryId_idx" ON "public"."ResponseSession"("juryId");
